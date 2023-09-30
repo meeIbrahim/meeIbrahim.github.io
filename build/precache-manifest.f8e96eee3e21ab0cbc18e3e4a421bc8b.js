ea84e76c795e5fd6202b915417403b2cd2b1630d
Muhammad Ibrahim Ahsan <meeibrahim39@gmail.com>
2023-09-30 14:26:11 +0500

    Build

---
+++ build/precache-manifest.f8e96eee3e21ab0cbc18e3e4a421bc8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b371910564c596f8aa6c",
+    "url": "/static/css/main.73229e9e.chunk.css"
+  },
+  {
+    "revision": "b371910564c596f8aa6c",
+    "url": "/static/js/main.b3719105.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "76d9cb6d00045414e0e33daceffab36f",
+    "url": "/index.html"
+  }
+];
