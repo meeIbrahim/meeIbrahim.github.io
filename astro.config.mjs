abb70f10c0f58e9f809b2f85e20f4b24fa8f7ab9
Ibrahim <ibrahim@arch.zephyrus.local>
2024-12-31 00:47:00 +0500

    add: pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: 'https://meeIbrahim.github.io'
+  site: 'https://meeIbrahim.github.io',
   });
