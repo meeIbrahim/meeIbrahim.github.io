67bbe65622f20f2d0bbde1ae8cf52145c19d5d2c
Ibrahim <ibrahim@arch.zephyrus.local>
2025-01-04 12:52:58 +0500

    fix: prettier fix

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,9 @@ import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {ignores: ["node_modules", "dist", "build", "out", "public", "src/env.d.ts"]},
+  {
+    ignores: ["node_modules", "dist", "build", "out", "public", "src/env.d.ts"],
+  },
   { files: ["**/*.{js,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
